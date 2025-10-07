202ae911e2042c1fcca6f635d4851ed50bfcdbb0
mr601s <marcus@bullishmindsai.co.site>
2025-10-07 17:51:13 -0400

    fix: resolve ESLint error in login page text

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -95,7 +95,7 @@ export default function LoginPage() {
         </div>
 
         <p className="text-center text-sm">
-          Don't have an account?{' '}
+          {"Don't have an account?"}{' '}
           <a className="text-blue-600 hover:underline" href="/signup">
             Sign Up
           </a>
